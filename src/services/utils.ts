22d0594eb6ff03385a38c435c4c19da796eead6f
paullyu7 <lyu.xia@northeastern.edu>
2023-03-19 15:32:15 -0400

    rephrased ski subtitle for summer months

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,6 +1,6 @@
 export function getSkiSubtitle(d: Date) {
     if (d.getMonth() > 5 && d.getMonth() < 12) {
-        return `It's literally ${monthToName(d.getMonth())}, you might want to rethink that.`
+        return `It's literally ${monthToName(d.getMonth())}, you might want to reconsider.`
     } else {
         return `It's prime season baby.`
     }
