f9ac5176a25b3965a3a0c2153d2b6b95ec16dc76
paullyu7 <lyu.xia@northeastern.edu>
2023-03-20 14:27:34 -0400

    Add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,39 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            
+        <meta
+          name="og-title"
+          content="So you want to plan a ski trip in New England"
+        />
+            
+        <meta name="og:url" content="https://c4ds-sketch-3.vercel.app/" />
+            
+        <meta
+          name="og:image"
+          content="file:///Users/lyu/Desktop/Screen%20Shot%202023-03-20%20at%2014.16.44.png"
+        />
+            
+        <meta name="og:type" content="article" />
+            
+        <meta
+          name="og:description"
+          content="An interactive poll that helps you to find the best fit ski resort."
+        />
+            
+        <meta name="og:locale" content="en_US" />
+            
+        <meta
+          name="keywords"
+          content="New England, Ski Resorts, Lift Ticket, weather, temperature"
+        />
+            
+        <meta
+          name="author"
+          content="Danish Farooq and Xiangpei Lyu, Northeastern University"
+        />
+            <title>So you want to plan a ski trip in New England</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
