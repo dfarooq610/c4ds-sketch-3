ff956f0f01adf27bd94768d082e81a071513bf74
Danish Farooq <farooq.d@northeastern.edu>
2023-03-19 21:39:13 -0400

    add active to gradient button for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,9 +17,6 @@ export default function Home() {
         >
           So you want to plan a ski trip in New England
         </h1>
-        {/* <h3 className="text-slate-300 text-2xl lg:text-4xl m-3 text-center">
-          {getSkiSubtitle(new Date())}
-        </h3> */}
       </section>
       <NeonSkiSign />
       {/* from https://devdojo.com/tailwindcss/buttons */}
@@ -27,7 +24,10 @@ export default function Home() {
         onClick={() => router.push("/quiz")}
         className="relative inline-flex items-center justify-center lg:text-4xl text-2xl max-w-prose mt-16 mb-4  px-10 py-6 overflow-hidden font-bold font-tiltNeon text-white hover:text-[#0a1b35] rounded-md shadow-2xl group"
       >
-        <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-white via-sky-400 to-sky-400 group-hover:opacity-100"></span>
+        <span
+          className="absolute inset-0 w-full h-full transition duration-500 ease-out opacity-0 
+        bg-gradient-to-br from-white via-sky-400 to-sky-400 group-hover:opacity-100 group-active:opacity-100"
+        ></span>
         {/* <!-- Top gradient glass effect --> */}
         <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
         {/* <!-- Bottom gradient glass effect--> */}
@@ -37,7 +37,7 @@ export default function Home() {
         {/* <!-- Right gradient glass effect --> */}
         <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
         <span className="absolute inset-0 w-full h-full border-4 border-white rounded-md opacity-10"></span>
-        <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+        <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 group-active:w-56 group-active:h-56 opacity-5"></span>
         <span className="relative text-xl lg:text-2xl">
           Let us give you some recs
         </span>
