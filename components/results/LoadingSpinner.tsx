c0d992d75cb735428f8043b7c969ce2361717262
Danish Farooq <farooq.d@northeastern.edu>
2023-03-21 07:03:13 -0400

    loading spinner addition for real

---
+++ components/results/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+export function LoadingSpinner() {
+
+  // sourced from https://cssloaders.github.io/
+  return (
+    <div className="m-auto">
+      <span className="loader"></span>
+    </div>
+  );
+}
